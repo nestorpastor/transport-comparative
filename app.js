b0857bf5b191fc6611a54271d7335b623840af1d
Nestor Pastor <nestorpastor10@gmail.com>
2021-09-07 17:09:14 -0300

    adding cors

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
+// Requires
+var express = require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Init var
+var app = express();
+var server = require('http').createServer(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+// CORS
+const cors = require('cors');
+app.use(cors());
+
+
+// importar rutas
+var appRoutes = require('./routes/app');
+
+
+app.use('/', appRoutes);
+
+var PORT = process.env.PORT || 8080;
+//var PORT = 3000;
+server.listen(PORT, () => {
+  console.log('Node/Express: \x1b[32m%s\x1b[0m', 'online - port: ' + PORT);
+});
---
+++ routes/app.js
@@ -0,0 +1,16 @@
+// Requires
+var express = require('express');
+
+// Init var
+var app = express();
+
+// Rutas
+app.get('/', (req, res, next) => {
+
+  res.status(200).json({
+    ok: true,
+    mensaje: 'Peticion realizada correctamente'
+  });
+
+});
+module.exports = app;
