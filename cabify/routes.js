0fb5172a126707d645bafca198612f4b9843f41a
Nestor Pastor <nestorpastor10@gmail.com>
2021-09-11 15:25:19 -0300

    cabify prod version

--- cabify/routes.js
+++ cabify/routes.js
@@ -1,7 +1,7 @@
 var { request, GraphQLClient, gql } = require('graphql-request');
 
 module.exports = async function getRoutePrices(from, to) {
-  const endpoint = 'https://cabify-sandbox.com/api/v3/graphql';
+  const endpoint = 'https://cabify.com/api/v3/graphql';
 
   const client = new GraphQLClient(endpoint)
 
@@ -9,7 +9,9 @@ module.exports = async function getRoutePrices(from, to) {
   estimates(estimateInput: $estimateInput){
     eta {
       min,
-      max
+      max,
+      formatted,
+      lowAvailability
     }
     
     total {
@@ -39,10 +41,10 @@ module.exports = async function getRoutePrices(from, to) {
       id
       icon
       description {
-        en
+        es
       }
     }
-    
+
     distance
     duration
     route
@@ -51,7 +53,7 @@ module.exports = async function getRoutePrices(from, to) {
   const variables = {
     estimateInput: {
       startType: 'ASAP',
-      requesterId: 'f7f551c4130b11ec84f99e152e4c0dfb',
+      requesterId: '6a0323ce131b11ecac2c0242ac110004',
       startAt: null,
       stops: [{
         loc: from
@@ -63,15 +65,16 @@ module.exports = async function getRoutePrices(from, to) {
   }
 
   const requestHeaders = {
-    authorization: 'Bearer R4PFnWnzBKWByKpgC-kMlwUp7vvGNh'
+    authorization: 'Bearer NXHLn_7F-GsXZOxP2D5BIFaTAitVu8'
   }
   const data = await client.request(query, variables, requestHeaders).then((data) => {
     return {
       app: 'cabify',
       low_estimate: getValorMasBajo(data.estimates),
       high_estimate: getValorMasAlto(data.estimates),
-      duration: ((data.estimates[0].eta.min + data.estimates[0].eta.max) / 2),
-      route: data.estimates[0].route
+      duration: (((data.estimates[0].eta.min + data.estimates[0].eta.max) / 2) / 60),
+      route: data.estimates[0].route,
+      currency: data.estimates[0].priceBase.currency
     }
   }).catch((err) => { return { error: { message: err.response.errors[0].friendly_message } } })
   return data;
@@ -87,7 +90,7 @@ function getValorMasBajo(precios) {
     }
   }
 
-  return valormasBajo
+  return valormasBajo / 100
 }
 
 function getValorMasAlto(precios) {
@@ -99,5 +102,5 @@ function getValorMasAlto(precios) {
     }
   }
 
-  return valorMasAlto
+  return valorMasAlto / 100
 }
