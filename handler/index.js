a62bcf3513ca8f8fcaf2c887c7348da3750c3903
Nestor Pastor <nestorpastor10@gmail.com>
2021-09-11 15:38:58 -0300

    adding uber to main response

--- handler/index.js
+++ handler/index.js
@@ -1,13 +1,17 @@
-var getRoutePricesCabify = require('../cabify/routes')
-
+var getRoutePricesCabify = require('../cabify/routes');
+var getRoutePrices = require('../uber/price.estimator');
 
 module.exports = async function compare(from, to) {
   let response = [];
 
   let cabify = await getRoutePricesCabify(from, to);
-
   if (!cabify.error) {
     response.push(cabify);
+  }
+
+  let uber = await getRoutePrices();
+  if (uber) {
+    response.push(uber);
   }
 
   return response;
--- uber/price.estimator.js
+++ uber/price.estimator.js
@@ -13,67 +13,68 @@ curl -H 'Authorization: Bearer <USER_ACCESS_TOKEN>' \
 */
 
 module.exports = async function getRoutePrices(params) {
-    let endpoint = 'https://api.uber.com/v1.2/estimates/price?';
-    let startLatitude = 37.7752315;
-    let startLongitude = -122.418075;
-    let endLatitude = 37.7752415; 
-    let endLongitude = -122.518075;
-    let seatCount = 2;
-    let apiResponse;
-    if (process.env.TRANSPORT_COMPARATIVE_UBER_ENDPOINT !== undefined) {
-        endpoint = process.env.TRANSPORT_COMPARATIVE_UBER_ENDPOINT;
+  let endpoint = 'https://api.uber.com/v1.2/estimates/price?';
+  let startLatitude = 37.7752315;
+  let startLongitude = -122.418075;
+  let endLatitude = 37.7752415;
+  let endLongitude = -122.518075;
+  let seatCount = 2;
+  let apiResponse;
+  if (process.env.TRANSPORT_COMPARATIVE_UBER_ENDPOINT !== undefined) {
+    endpoint = process.env.TRANSPORT_COMPARATIVE_UBER_ENDPOINT;
+  }
+  if (params !== undefined) {
+    if (params.startLatitude != undefined) {
+      startLatitude = params.startLatitude;
     }
-    if (params !== undefined) {
-        if (params.startLatitude != undefined) {
-            startLatitude = params.startLatitude;
-        }
-        if (params.startLongitude != undefined) {
-            startLongitude = params.startLatitude;
-        }
-        if (params.endLatitude != undefined) {
-            endLatitude = params.endLatitude;
-        }
-        if (params.endLongitude != undefined) {
-            endLongitude = params.endLatitude;
-        }
-        if (params.seatCount != undefined) {
-            seatCount = params.seatCount;
-        }
+    if (params.startLongitude != undefined) {
+      startLongitude = params.startLatitude;
     }
-    let request = `${endpoint}start_latitude=${startLatitude}&start_longitude=${startLongitude}&end_latitude=${endLatitude}&end_longitude=${endLongitude}&seat_count=${seatCount}`;
-    console.log(`request: ${request}`)
-    if(process.env.TRANSPORT_COMPARATIVE_UBER_USE_MOCK !== undefined) {
-        apiResponse = JSON.parse(fs.readFileSync(filePath));
+    if (params.endLatitude != undefined) {
+      endLatitude = params.endLatitude;
+    }
+    if (params.endLongitude != undefined) {
+      endLongitude = params.endLatitude;
+    }
+    if (params.seatCount != undefined) {
+      seatCount = params.seatCount;
+    }
+  }
+  let request = `${endpoint}start_latitude=${startLatitude}&start_longitude=${startLongitude}&end_latitude=${endLatitude}&end_longitude=${endLongitude}&seat_count=${seatCount}`;
+  console.log(`request: ${request}`)
+  if (process.env.TRANSPORT_COMPARATIVE_UBER_USE_MOCK !== undefined) {
+    apiResponse = JSON.parse(fs.readFileSync(filePath));
+    console.log(`Result ${apiResponse.prices[0]}`)
+    return {
+      app: 'uber',
+      high_estimate: apiResponse.prices[0].high_estimate,
+      low_estimate: apiResponse.prices[0].low_estimate,
+      duration: apiResponse.prices[0].duration,
+      route: '',
+      currency: apiResponse.prices[0].currency_code
+    }
+  } else {
+    // Call Uber API
+    https.get(request, () => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        apiResponse = JSON.parse(data);
         console.log(`Result ${apiResponse.prices[0]}`)
         return {
-            app: 'uber',
-            high_estimate: apiResponse.prices[0].high_estimate,
-            low_estimate: apiResponse.prices[0].low_estimate,
-            duration: apiResponse.prices[0].duration,
-            route:''
+          app: 'uber',
+          high_estimate: apiResponse.prices[0].high_estimate,
+          low_estimate: apiResponse.prices[0].low_estimate,
+          duration: apiResponse.prices[0].duration,
+          route: ''
         }
-    } else {
-        // Call Uber API
-        https.get(request, () =>{
-            let data = '';
-
-            response.on('data', (chunk) => {
-              data += chunk;
-            });
-          
-            response.on('end', () => {
-                apiResponse = JSON.parse(data);
-                console.log(`Result ${apiResponse.prices[0]}`)
-                return {
-                    app: 'uber',
-                    high_estimate: apiResponse.prices[0].high_estimate,
-                    low_estimate: apiResponse.prices[0].low_estimate,
-                    duration: apiResponse.prices[0].duration,
-                    route:''
-                }
-            });
-        }).on('error', (error) => {
-            console.log("Error: " + error.message);
-        })
-    }
+      });
+    }).on('error', (error) => {
+      console.log("Error: " + error.message);
+    })
+  }
 }
